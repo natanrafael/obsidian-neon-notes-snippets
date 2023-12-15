adff8ead4c4506a54909802ee0a9ccc39ed3c3c0
natanrafael <natanrafael.b@gmail.com>
2023-12-14 23:06:19 -0300

    feat: add pulsing canvas effect
    this commit also contains the implementation of a ps1 file to facilitate the compiling of the snippets

--- commands.md
+++
@@ -1,4 +0,0 @@
-# HEADING WITH TERMINAL CURSOR
-sass --no-source-map --style=compressed src\heading-with-terminal-cursor\main.scss "Heading with terminal cursor.css"
-# INLINE TITLE GLITCH
-sass --no-source-map --style=compressed src\inline-title-glitch\main.scss "Inline-title Glitch.css" 
---
+++ compile.ps1
@@ -0,0 +1,3 @@
+sass --no-source-map --style=compressed src\pulsing-canvas-grid\main.scss:pulsing-canvas-grid.css;
+sass --no-source-map --style=compressed src\inline-title-glitch\main.scss:inline-title-glitch.css;
+sass --no-source-map --style=compressed src\heading-with-terminal-cursor\main.scss:heading-with-terminal-cursor.css;
